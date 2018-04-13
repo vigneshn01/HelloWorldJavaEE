6277f3b91ac15e7ba2e18a2694db65fd1bc8789e
vigneshn01 <38283639+vigneshn01@users.noreply.github.com>
2018-04-13 14:00:02 +0530

    Create AppTest.java

---
+++ src/test/java/com/qaagility/javaee/AppTest.java
@@ -0,0 +1,30 @@
+
+package com.qaagility.javaee;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import com.qaagility.javaee.Calculator;
+ 
+
+public class AppTest {
+
+Calculator myCalc = new Calculator();
+
+      public void testSlow() {
+        System.out.println("slow");
+      }
+
+      public void testSlower() {
+        System.out.println("slower");
+      }
+
+      public void testFast() {
+        System.out.println("fast-updated");
+      }
+	
+	@Test	
+      public void testCalc() {
+        assertEquals("Result", 9, myCalc.add(3,6));
+      }
+
+}
